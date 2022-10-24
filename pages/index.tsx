07209cbb65c66010102dc0cde2e266b686c63b0a
wachira_Dev <31078302+tesh254@users.noreply.github.com>
2022-10-25 00:40:18 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Nav from "../components/Nav";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+          Next.js + Tailwind CSS 2.0
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
